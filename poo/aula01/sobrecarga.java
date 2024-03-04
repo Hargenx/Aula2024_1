3e1012a3ddcf4331a442900a19df1c0ed7d5d44e
Raphael <Hargenx@hotmail.com>
2024-03-03 22:38:32 -0300

    Funny

---
+++ poo/aula01/sobrecarga.java
@@ -0,0 +1,34 @@
+package poo.aula01;
+
+class Animal {
+    public void fazerSom() {
+        System.out.println("Som do Animal");
+    }
+}
+
+class Cachorro extends Animal {
+    @Override
+    public void fazerSom() {
+        System.out.println("Latindo... Woof! Woof!");
+    }
+}
+
+class Gato extends Animal {
+    @Override
+    public void fazerSom() {
+        System.out.println("Miando... Meow!");
+    }
+}
+
+public class sobrecarga {
+    public static void main(String[] args) {
+        Animal[] animais = new Animal[2];
+
+        animais[0] = new Cachorro();
+        animais[1] = new Gato();
+
+        for (Animal animal : animais) {
+            animal.fazerSom();
+        }
+    }
+}
