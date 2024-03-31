097a46add683db5073079f341bc9679db6b886e4
Raphael <Hargenx@hotmail.com>
2024-03-30 23:15:52 -0300

    Exemplo PWA Exemplos Java manipulação arquivos e uso de ferramentas Exercicios Java

---
+++ poo/aula_extra_gravaArquivo_Regex/RegexExemplo.java
@@ -0,0 +1,28 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class RegexExemplo {
+    public static void main(String[] args) {
+        String texto = "O professor da disciplina de Programação orientada a Objeto JAVA é o professor Raphael.";
+        Pattern pattern = Pattern.compile("\\bRaphael\\b");
+        Matcher matcher = pattern.matcher(texto);
+        while (matcher.find()) {
+            System.out.println("Encontrou a palavra 'Raphael' em: " + matcher.start());
+        }
+        // Texto de exemplo com um e-mail válido e um inválido
+        String texto2 = "O e-mail do usuário é raphael@email.com e o outro é invalido@teste";
+
+        // Expressão regular para validar e-mails
+        String regexEmail = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b";
+
+        // Compilar a expressão regular em um padrão
+        Pattern patternEmail = Pattern.compile(regexEmail);
+
+        // Criar um matcher para encontrar correspondências na string de texto
+        Matcher matcherEmail = patternEmail.matcher(texto2);
+
+        // Iterar sobre as correspondências encontradas
+        while (matcherEmail.find()) {
+            System.out.println("E-mail encontrado: " + matcherEmail.group());
+        }
+    }
+}
---
+++ poo/exercicios/exercicio01/Main.java
@@ -0,0 +1,45 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+public class Main {
+    public static void salvarEmTxt(List<Pessoa> pessoas, String nomeArquivo) {
+        FileWriter arquivo = null;
+        try {
+            arquivo = new FileWriter(nomeArquivo);
+            for (Pessoa pessoa : pessoas) {
+                arquivo.write("Nome: " + pessoa.nome + ", Idade: " + pessoa.idade + "\n");
+            }
+            System.out.println("Dados salvos em pessoas.txt: " + nomeArquivo);
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar em pessoas.txt: " + e.getMessage());
+        } finally {
+            try {
+                if (arquivo != null) {
+                    arquivo.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Erro ao fechar o arquivo: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        pessoas.add(new Pessoa("Raphael", 39));
+        pessoas.add(new Pessoa("Caroline", 30));
+        String diretorioAtual = System.getProperty("user.dir") + System.getProperty("file.separator") + "pessoas.txt";
+        salvarEmTxt(pessoas, diretorioAtual);
+    }
+}
