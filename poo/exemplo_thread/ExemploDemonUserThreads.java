a1ee830e47c30946f60b05e1da66e15fa89c7efe
Raphael <Hargenx@hotmail.com>
2024-04-07 21:23:55 -0300

    Exemplos para aula Java com Threads

---
+++ poo/exemplo_thread/ExemploDemonUserThreads.java
@@ -0,0 +1,38 @@
+public class ExemploDemonUserThreads {
+    public static void main(String[] args) {
+        // Criando uma thread de usuário
+        Thread threadUser = new Thread(new MinhaThread("Thread de Usuário"));
+        // Definindo como user thread (padrão)
+        threadUser.setDaemon(false);
+        threadUser.start();
+
+        // Criando uma thread demoníaca
+        Thread threadDemon = new Thread(new MinhaThread("Thread Demoníaca"));
+        // Definindo como daemon thread
+        threadDemon.setDaemon(true);
+        threadDemon.start();
+
+        System.out.println("Fim do método main");
+    }
+}
+
+class MinhaThread implements Runnable {
+    private String nome;
+
+    public MinhaThread(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(nome + ": Mensagem " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(nome + " terminou.");
+    }
+}
